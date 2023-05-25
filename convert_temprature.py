febe8cc7895b6f56107b949c0369f66d5492e9b5
Ayda Arami <aramiayda95@gmail.com>
2023-05-25 14:24:06 +0200

    docstring for source code documentation

--- convert_temprature.py
+++ convert_temprature.py
@@ -1,4 +1,12 @@
 def fahrenheit_to_celsius (temperature_fahrenheit)
+	"""
+	convert temprature in fahreheit to celsius
+	Parameters:
+	temprature_fahremheit
+
+	Returs:
+	Tempreature_Celsius
+	"""
 	c_temp=(temperature_fahrenheit-32)*(5.0/9.0)
 	return c_temp
 
