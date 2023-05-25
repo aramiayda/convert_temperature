20b3706bf5731cd259cf2094df0d492cdd15943b
Ayda Arami <aramiayda95@gmail.com>
2023-05-25 13:44:16 +0200

    explicit naming is better than implicit naming

--- convert_temprature.py
+++ convert_temprature.py
@@ -1,4 +1,4 @@
-def fahrenheit_to_celsius (temp_fahrenheit)
-	c_temp=(temp_fahrenheit-32)*(5.0/9.0)
+def fahrenheit_to_celsius (temperature_fahrenheit)
+	c_temp=(temperature_fahrenheit-32)*(5.0/9.0)
 	return c_temp
 
